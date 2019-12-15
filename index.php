2dada1ab4cd7724f8cd743f1bf0f00bac8ded2d9
Morioka Naoya <g1989n@gmail.com>
2019-12-15 11:27:07 +0900

    レイアウトを入れる（ログインテンプレート利用）

--- index.php
+++ index.php
@@ -11,66 +11,85 @@
 <meta name="ROBOTS" content="NOINDEX,NOFOLLOW">
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <title>blog</title>
-	
-<style>
-body{background:#00441F;color:#fff;font-family: YuGothic, '游ゴシック', 'Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3', Osaka, 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', Tahoma, Verdana, Arial, Verdana, sans-serif;
-	font-size:0.9em;line-height:1.1em;padding:10px;box-sizing: border-box;}	
-a{color:#ffffcc;text-decoration:none;font-weight: normal;}
-#wrap{width:100%;border:none;margin:0;}
-.thumbnail{height:inherit;}
-@media only screen and (min-width: 334px) {
-	body{background:#00441F;color:#fff;font-family: YuGothic, '游ゴシック', 'Hiragino Kaku Gothic Pro', 'ヒラギノ角ゴ Pro W3', Osaka, 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', Tahoma, Verdana, Arial, Verdana, sans-serif;
-		font-size:12px;line-height:16px;padding:10px;box-sizing: border-box;
-	}
-}
-@media only screen and (min-width: 768px) {
-	#wrap{width:1000px;border:solid 1px #ccc;margin:10px auto;}
-	.thumbnail{height:150px;}
-}
-</style>
 
+<link href="./css/style1.css" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.12.1.min.js"></script>
-
-<!-- BootStrap -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<!-- // BootStrap -->
+<!------ Include the above in your HEAD tag ---------->
 
 <script type="text/javascript">
-    /* https://www.sejuku.net/blog/42985#post-3 */
     $(function(){
         $('form').submit(function(event){
             event.preventDefault();
-            //console.log('submit');
-            //console.log(event);
-            //var form = $(this);
-            //console.log(form)
-            //console.log($(this));
-            //console.log($(this).attr('action'));
-            //console.log($(this).serializeArray());
             $.post({
                 url: $(this).attr('action'),
                 data: $(this).serialize(),
-                /*dataType: 'json',*/
             }).done(function(data){
-                //console.log(data);
+                console.log(data);
+                outputHtml = '<div class="alert alert-success">あなたのマスターナンバーは ' + data + ' です</div>';
+                $('#form-result').html(outputHtml);
             }).fail(function(a,b,c){
-                /* console.log('error');
-                console.log(a);
-                console.log(b);
-                console.log(c); */
+                //
             }).always(function(){
-                // console.log('always');
+                //
             })
         });
     })
     /* 複数ボタンはこちら（https://qiita.com/icbmuma/items/92f3467a54a071280595） */
 </script>
+
 </head>
 <body>
-<form action="master-number.php" method="post">
-<p>
-名前：<input type="text" name="name" size="40">
-</p>
-<input type="submit" value="送信">
+<!--
+User Profile Sidebar by @keenthemes
+A component of Metronic Theme - #1 Selling Bootstrap 3 Admin Theme in Themeforest: http://j.mp/metronictheme
+Licensed under MIT
+-->
+
+<div class="container">
+	<div class="page-lock">
+        <div class="page-logo">
+    		<a class="brand" href="index.html">
+    		<img src="./img/masternumberLogo2_60x60.png" alt="logo"/>
+    		</a>
+    	</div>
+    	<div class="page-body">
+    		<div class="lock-head">
+    			 マスターナンバー
+    		</div>
+    		<div class="lock-body">
+    			<div class="pull-left lock-avatar-block">
+    				<!--<img src="./img/masternumberLogo.png" class="lock-avatar">-->
+    			</div>
+    			<form class="lock-form pull-left" action="master-number.php" method="post">
+    				<h4>下記にあなたのお名前をローマ字で入力してください。</h4>
+                    <p class="text-muted">（例：山田太郎 → YAMADA TAROU）</p>
+    				<div class="form-group">
+    					<!--<input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password"/>-->
+                        <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="YAMADA TAROU" name="name" required/>
+    				</div>
+    				<div class="form-actions">
+    					<button type="submit" class="btn btn-success uppercase">判定する！</button>
+    				</div>
+                    <hr class="" />
+                    <div id="form-result">
+                    </div>
+    			</form>
+    		</div>
+    		<div class="lock-bottom">
+    			<a href="">詳しくはこちら</a>
+    		</div>
+    	</div>
+    </div>
+</div>
+
+<br>
+<br>
+<center>
+<strong>Designed by <a href="http://j.mp/metronictheme" target="_blank">KeenThemes</a></strong>
+</center>
+<br>
+<br>
 </body>
+</html>
